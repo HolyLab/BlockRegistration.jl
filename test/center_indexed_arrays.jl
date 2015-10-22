c38e2a9de3cd85af9a31b2e0af1b370e4b5df3c9
Tim Holy <tim.holy@gmail.com>
2015-10-22 10:52:25 -0500

    Rename array data->dat to avoid conflict with Images.data

--- test/center_indexed_arrays.jl
+++ test/center_indexed_arrays.jl
@@ -4,31 +4,31 @@ using CenterIndexedArrays, Base.Test
 CenterIndexedArray(Float32, 3, 5)
 @test_throws ErrorException CenterIndexedArray(Float32, 4, 5)
 
-data = rand(3,5)
-A = CenterIndexedArray(data)
-@test size(A) == size(data)
-@test length(A) == length(data)
+dat = rand(3,5)
+A = CenterIndexedArray(dat)
+@test size(A) == size(dat)
+@test length(A) == length(dat)
 @test ndims(A) == 2
-@test eltype(A) == eltype(data)
+@test eltype(A) == eltype(dat)
 
-@test A[0,0] == data[2,3]
+@test A[0,0] == dat[2,3]
 k = 0
 for j = -2:2, i = -1:1
     k += 1
-    @test A[i,j] == data[k]
+    @test A[i,j] == dat[k]
 end
 @test_throws BoundsError A[3,5]
-@test A[:,-1:1].data == data[:,2:4]
-@test A[-1:1,:].data == data[:,:]
+@test A[:,-1:1].data == dat[:,2:4]
+@test A[-1:1,:].data == dat[:,:]
 @test_throws ErrorException A[:,-2:0]
 k = 0
 for j = -2:2, i = -1:1
     A[i,j] = (k+=1)
 end
-@test data == reshape(1:15, 3, 5)
+@test dat == reshape(1:15, 3, 5)
 @test_throws BoundsError A[3,5] = 15
 
-rand!(data)
+rand!(dat)
 iall = (-1:1).*ones(Int, 5)'
 jall = ones(Int,3).*(-2:2)'
 k = 0
@@ -44,36 +44,36 @@ str = takebuf_string(io)
 @test isempty(search(str, "undef"))
 
 # Iteration
-for (a,d) in zip(A, data)
+for (a,d) in zip(A, dat)
     @test a == d
 end
 
 # Standard julia operations
 B = copy(A)
 
-@test B == data
+@test B == dat
 @test B == A
-@test isequal(B, data)
+@test isequal(B, dat)
 @test isequal(B, A)
 
-@test vec(A) == vec(data)
+@test vec(A) == vec(dat)
 
-@test minimum(A) == minimum(data)
-@test maximum(A) == maximum(data)
-@test minimum(A,1) == minimum(data,1)
-@test maximum(A,2) == maximum(data,2)
+@test minimum(A) == minimum(dat)
+@test maximum(A) == maximum(dat)
+@test minimum(A,1) == minimum(dat,1)
+@test maximum(A,2) == maximum(dat,2)
 
 amin, iamin = findmin(A)
-dmin, idmin = findmin(data)
+dmin, idmin = findmin(dat)
 @test amin == dmin
 @test A[iamin] == amin
-@test amin == data[idmin]
+@test amin == dat[idmin]
 
 amax, iamax = findmax(A)
-dmax, idmax = findmax(data)
+dmax, idmax = findmax(dat)
 @test amax == dmax
 @test A[iamax] == amax
-@test amax == data[idmax]
+@test amax == dat[idmax]
 
 fill!(A, 2)
 @test all(x->x==2, A)
@@ -82,23 +82,23 @@ i, j = findn(A)
 @test vec(i) == vec(iall)
 @test vec(j) == vec(jall)
 
-rand!(data)
+rand!(dat)
 
-@test cat(1, A, data) == cat(1, data, data)
-@test cat(2, A, data) == cat(2, data, data)
+@test cat(1, A, dat) == cat(1, dat, dat)
+@test cat(2, A, dat) == cat(2, dat, dat)
 
-@test permutedims(A, (2,1)) == permutedims(data, (2,1))
-@test ipermutedims(A, (2,1)) == ipermutedims(data, (2,1))
+@test permutedims(A, (2,1)) == permutedims(dat, (2,1))
+@test ipermutedims(A, (2,1)) == ipermutedims(dat, (2,1))
 
-@test cumsum(A, 1) == cumsum(data, 1)
-@test cumsum(A, 2) == cumsum(data, 2)
+@test cumsum(A, 1) == cumsum(dat, 1)
+@test cumsum(A, 2) == cumsum(dat, 2)
 
-@test mapslices(v->sort(v), A, 1) == mapslices(v->sort(v), data, 1)
-@test mapslices(v->sort(v), A, 2) == mapslices(v->sort(v), data, 2)
+@test mapslices(v->sort(v), A, 1) == mapslices(v->sort(v), dat, 1)
+@test mapslices(v->sort(v), A, 2) == mapslices(v->sort(v), dat, 2)
 
-@test flipdim(A, 1) == flipdim(data, 1)
-@test flipdim(A, 2) == flipdim(data, 2)
+@test flipdim(A, 1) == flipdim(dat, 1)
+@test flipdim(A, 2) == flipdim(dat, 2)
 
-@test A + 1 == data + 1
-@test 2*A == 2*data
-@test A+A == data+data
+@test A + 1 == dat + 1
+@test 2*A == 2*dat
+@test A+A == dat+dat
