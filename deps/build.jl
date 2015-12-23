972542f907fe4e9865a0a3bff75e882f1c7b3879
Tim Holy <tim.holy@gmail.com>
2015-12-22 20:13:42 -0600

    Update for new MathProgBase API

--- deps/build.jl
+++ deps/build.jl
@@ -2,6 +2,7 @@ if Base.find_in_path("RFFT") == nothing
     Pkg.clone("git@github.com:HolyLab/RFFT.jl.git")
 end
 Pkg.checkout("FixedSizeArrays")
+Pkg.checkout("ForwardDiff")
 Pkg.checkout("Optim", "teh/constrained")
 
 basedir = splitdir(splitdir(@__FILE__)[1])[1]
